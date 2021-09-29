871d6252c0f35741223417b436af09a435783c76
Ayobami Ayodeji <aayodeji@microsoft.com>
2021-09-29 05:02:04 +0000

    testing

--- Scenarios/Secure-Baseline/Terraform/03-AAD-import/variables.tf
+++ Scenarios/Secure-Baseline/Terraform/03-AAD-import/variables.tf
@@ -3,11 +3,11 @@
 #############
 
 variable "aks_admin_group" {
-    default = "AKS App Admin Team"
+    default = "AKS App Admin Team 2"
 
 }
 
 variable "aks_user_group" {
-    default = "AKS App Dev Team"
+    default = "AKS App Dev Team 2"
 
 }
