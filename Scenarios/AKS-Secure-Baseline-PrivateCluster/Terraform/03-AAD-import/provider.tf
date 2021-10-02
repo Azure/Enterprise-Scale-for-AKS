4ba3e07a07b8f077ef210b01c40df1bb380d041c
ArielRam99 <ariel.ramirez@microsoft.com>
2021-10-01 18:25:10 -0700

    adding vars

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/provider.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/provider.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.46.1"
+      version = ">= 2.79.0"
     }
 
   }
@@ -21,6 +21,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id        = var.client_id
+  client_secret    = var.client_secret
+  subscription_id  = var.subscription_id
+  tenant_tenant_id = var.tenant_id
 }
 
 provider "azuread" {
--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/variables.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-import/variables.tf
@@ -11,3 +11,15 @@ variable "aks_user_group" {
     default = "AKS App Dev Team"
 
 }
+
+variable "client_id" {
+}
+
+variable "client_secret" {
+}
+
+variable "subscription_id" {
+}
+
+variable "tenant_id" {
+}
