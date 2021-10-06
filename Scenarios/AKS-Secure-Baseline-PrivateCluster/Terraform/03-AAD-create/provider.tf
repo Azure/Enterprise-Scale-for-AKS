b55059a95626db5ad57dbe8c87039d1f05f63a34
ArielRam99 <ariel.ramirez@microsoft.com>
2021-10-06 11:58:57 -0700

    azuread block

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/provider.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/provider.tf
@@ -7,6 +7,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.79.1"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.6.0"
+    }
 
   }
 
