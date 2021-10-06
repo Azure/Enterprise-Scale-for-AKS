0faa476b3a341827f6453ed584648cad8dbdc0b4
ArielRam99 <ariel.ramirez@microsoft.com>
2021-10-06 11:42:00 -0700

    comment out outputs

--- Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/ad_groups.tf
+++ Scenarios/AKS-Secure-Baseline-PrivateCluster/Terraform/03-AAD-create/ad_groups.tf
@@ -4,22 +4,19 @@
 
  resource "azuread_group" "appdevs" {
    display_name = var.aks_admin_group
-   security_enabled = false
+   security_enabled = true
  }
 
 
  resource "azuread_group" "aksops" {
    display_name = var.aks_user_group
-   security_enabled = false
+   security_enabled = true
  }
 
- output "appdev_object_id" {
-     value = azuread_group.appdevs.object_id
- }
-
- output "aksops_object_id" {
-     value = azuread_group.aksops.object_id
- }
-
-
-
+ #output "appdev_object_id" {
+ #    value = azuread_group.appdevs.object_id
+ #}
+#
+ #output "aksops_object_id" {
+ #    value = azuread_group.aksops.object_id
+ #}
